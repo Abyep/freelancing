e727046c0971f3bcafe268008555b075f582073a
Abhishek <abhishek@safearth.in>
2020-05-29 09:42:34 +0530

    freelancing homepage

---
+++ build/precache-manifest.da74f0ae9066becd6ef8328a0480f558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dc811936af6428e1884ffaf25dc5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32d71715f8ee9bff2385",
+    "url": "/static/css/main.4f9e0e22.chunk.css"
+  },
+  {
+    "revision": "03fd871d30f8b3e21223",
+    "url": "/static/js/2.185bc6f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.185bc6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d71715f8ee9bff2385",
+    "url": "/static/js/main.dd50fa52.chunk.js"
+  },
+  {
+    "revision": "f5fc6d693a7e40cf8e98",
+    "url": "/static/js/runtime-main.92ec87da.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getData } from "./Saga";
+import FormControl from "@material-ui/core/FormControl";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import { Paper, TextField } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      filteredData: [],
+      category: "",
+      searchInput: "",
+      categories: ["first_name", "last_name", "email"],
+    };
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getData());
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.data !== null) {
+      return { data: nextProps.data, filteredData: nextProps.data.data };
+    } else return null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.data.length !== 0) {
+      return true;
+    }
+    return true;
+  }
+
+  handleCategory = (event) => {
+    this.setState({
+      category: event.target.value.toLowerCase(),
+    });
+  };
+
+  handleSearch = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+  };
+
+  Capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  search = () => {
+    return (
+      <div className="searchDiv">
+        <FormControl>
+          <TextField
+            variant="outlined"
+            select
+            style={{ width: "250px" }}
+            native
+            onChange={this.handleCategory}
+            label="Select Category"
+          >
+            <option aria-label="None" value="" />
+            {this.state.categories.map((category, index) => (
+              <option key={index} value={category}>
+                {this.Capitalize(category.replace("_", " "))}
+              </option>
+            ))}
+          </TextField>
+        </FormControl>
+        <TextField
+          variant="outlined"
+          value={this.state.searchInput}
+          placeholder="Search"
+          id="standard-start-adornment"
+          onChange={this.handleSearch}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+    );
+  };
+
+  handleId = (id) => {
+    this.setState({
+      id: id,
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    let filteredData = this.state.filteredData.slice();
+    if (this.state.searchInput !== "") {
+      filteredData = this.state.filteredData.filter((data) => {
+        return (
+          data[this.state.category]
+            .toLowerCase()
+            .indexOf(this.state.searchInput.toLowerCase()) !== -1
+        );
+      });
+    }
+
+    return (
+      <div>
+        <Paper className="paper">
+          {this.search()}
+          <div className="parentDiv">
+            {this.props.data &&
+              filteredData.map((user, index) => (
+                <div key={user.id} className="userDiv">
+                  <img src={user.avatar} className="userIcon" />
+                  <div className="userInfo">
+                    <div>
+                      {user.first_name}+ +{user.last_name}
+                    </div>
+                    <div>{user.email}</div>
+                  </div>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        value={this.state.id}
+                        checked={this.state.id}
+                        onChange={() => this.handleId(user.id)}
+                      />
+                    }
+                  />
+                </div>
+              ))}
+          </div>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    data: state.reducer.data,
+  };
+};
+
+export default connect(mapStateToProps)(App);
